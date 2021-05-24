b4bf3e773cc93c1f3b954cdd152c96ce3d19efd3
Maksym Nazarenko <maks.nazarenko@gmail.com>
2021-05-24 14:24:38 +0300

    add rooms

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 PROJECT_ROOT = $(shell git rev-parse --show-toplevel)
+
 build-chat:
-	@go build -o ./cmd/chat/chat ./cmd/chat
+	@go build -o $(PROJECT_ROOT)/cmd/chat/chat $(PROJECT_ROOT)/cmd/chat
+
+run-chat: build-chat
+	TEMPLATES_DIR=$(PROJECT_ROOT)/cmd/chat/templates \
+		$(PROJECT_ROOT)/cmd/chat/chat
--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+)
+
+const (
+	TEMPLATES_DIR_ENV_NAME = "TEMPLATES_DIR"
 )
 
 type templateHandler struct {
@@ -16,13 +21,22 @@ type templateHandler struct {
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.tpl = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		var templatesDir string
+		var ok bool
+		if templatesDir, ok = os.LookupEnv(TEMPLATES_DIR_ENV_NAME); !ok {
+			templatesDir = "templates"
+		}
+
+		t.tpl = template.Must(template.ParseFiles(filepath.Join(templatesDir, t.filename)))
 		t.tpl.Execute(w, nil)
 	})
 }
 
 func main() {
+	r := NewRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 
 	listenAddr := "127.0.0.1:8080"
 	log.Println("Starting server on ", listenAddr)
